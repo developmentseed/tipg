574c9e7ce8d907c905134d0c51c19500429e2ac5
vincentsarago <vincent.sarago@gmail.com>
2022-06-29 16:52:25 +0200

    add test for json output

--- tests/routes/test_item.py
+++ tests/routes/test_item.py
@@ -16,6 +16,15 @@ def test_item(app):
     assert "text/html" in response.headers["content-type"]
     assert "Collection Item: 1" in response.text
 
+    # json output
+    response = app.get("/collections/public.landsat_wrs/items/1?f=json")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+    feat = response.json()
+    assert ["colectionId", "itemId", "id", "pr", "row", "path", "ogc_fid"] == list(
+        feat.keys()
+    )
+
     # not found
     response = app.get("/collections/public.landsat_wrs/items/50000")
     assert response.status_code == 404
