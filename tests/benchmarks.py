ce546d8defa099a5c9d113402fcae31a7266aaf5
vincentsarago <vincent.sarago@gmail.com>
2022-11-14 17:11:01 +0100

    do proper benchmark

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -19,15 +19,18 @@ def test_benchmark_items(benchmark, format, limit, app):
 
 
 @pytest.mark.parametrize("name", ["NewfoundlandandLabrador", "Saskatchewan"])
-def test_benchmark_item(benchmark, name, app):
+@pytest.mark.parametrize("format", ["geojson", "html"])
+def test_benchmark_item(benchmark, format, name, app):
     """Benchmark big item."""
 
-    params = {"f": "geojson", "name": name}
+    params = {"f": format, "prnom": name}
 
     def f(p):
-        return app.get("/collections/public.landsat_wrs/items", params=p)
+        return app.get("/collections/public.canada/items", params=p)
 
     benchmark.group = "Big Feature"
 
     response = benchmark(f, params)
     assert response.status_code == 200
+    if format == "geojson":
+        assert response.json()["features"][0]["properties"]["prnom"] == name
