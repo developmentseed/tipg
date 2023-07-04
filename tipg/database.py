aa4eeb2ee3870421027ca074db47178625ecbcdf
Remco Meeuwissen <remco.meeuwissen@opusinsights.nl>
2023-07-04 13:09:36 +0200

    Linter fixes

--- tipg/database.py
+++ tipg/database.py
@@ -7,7 +7,7 @@ import orjson
 from buildpg import asyncpg
 
 from tipg.logger import logger
-from tipg.settings import PostgresSettings, DatabaseSettings
+from tipg.settings import DatabaseSettings, PostgresSettings
 
 from fastapi import FastAPI
 
@@ -20,6 +20,7 @@ except ImportError:
 DB_CATALOG_FILE = resources_files(__package__) / "sql" / "dbcatalog.sql"
 
 database_settings = DatabaseSettings()
+
 
 class connection_factory:
     """Connection creation."""
