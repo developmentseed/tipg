84d50ad9576f37da43517b6264faaeb0cf3025aa
Remco Meeuwissen <remco.meeuwissen@opusinsights.nl>
2023-07-13 19:26:07 +0200

    Extracted the render call into its own class

--- tipg/resources/response.py
+++ tipg/resources/response.py
@@ -14,29 +14,25 @@ def default(obj):
         return str(obj)
 
 
-class GeoJSONResponse(JSONResponse):
+class ORJSONResponse(JSONResponse):
+    """Custom response handler for using orjson"""
+
+    def render(self, content: Any) -> bytes:
+        """Render the content into a JSON response using orjson"""
+        return orjson.dumps(
+            content,
+            default=default,
+            option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY,
+        )
+
+
+class GeoJSONResponse(ORJSONResponse):
     """GeoJSON Response"""
 
     media_type = "application/geo+json"
 
-    def render(self, content: Any) -> bytes:
-        """Render the content into a JSON response using orjson"""
-        return orjson.dumps(
-            content,
-            default=default,
-            option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY,
-        )
 
-
-class SchemaJSONResponse(JSONResponse):
+class SchemaJSONResponse(ORJSONResponse):
     """Schema Response"""
 
     media_type = "application/schema+json"
-
-    def render(self, content: Any) -> bytes:
-        """Render the content into a JSON response using orjson"""
-        return orjson.dumps(
-            content,
-            default=default,
-            option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY,
-        )
