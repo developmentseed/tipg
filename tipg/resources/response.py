49132dafe5f2d32b26d8426dacbbfc11671aceee
Remco Meeuwissen <remco.meeuwissen@opusinsights.nl>
2023-07-13 18:33:49 +0200

    Fixed a bug where floats in the database would not be serialized correctly in the json response

--- tipg/resources/response.py
+++ tipg/resources/response.py
@@ -1,15 +1,42 @@
 """tipg custom responses."""
 
-from fastapi.responses import ORJSONResponse
+import decimal
+from typing import Any
+
+import orjson
+
+from fastapi.responses import JSONResponse
 
 
-class GeoJSONResponse(ORJSONResponse):
+def default(obj):
+    """Instruct orjson what to do with types it does not natively serialize"""
+    if isinstance(obj, decimal.Decimal):
+        return str(obj)
+
+
+class GeoJSONResponse(JSONResponse):
     """GeoJSON Response"""
 
     media_type = "application/geo+json"
 
+    def render(self, content: Any) -> bytes:
+        """Render the content into a JSON response using orjson"""
+        return orjson.dumps(
+            content,
+            default=default,
+            option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY,
+        )
 
-class SchemaJSONResponse(ORJSONResponse):
+
+class SchemaJSONResponse(JSONResponse):
     """Schema Response"""
 
     media_type = "application/schema+json"
+
+    def render(self, content: Any) -> bytes:
+        """Render the content into a JSON response using orjson"""
+        return orjson.dumps(
+            content,
+            default=default,
+            option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY,
+        )
